4a3c9c9520ceb13d26b1a0e75593877cb66f7675
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-11-03 11:53:52 +0530

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -29,12 +29,11 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+    <div className='hidden sm:block w-full h-auto absolute inset-0 z-[-1]'>
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
-
         <Preload all />
       </Canvas>
     </div>
