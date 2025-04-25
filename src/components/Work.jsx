dce01e74607a76a0e0b7cbf7f51ed8fc2545f60e
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2025-04-25 23:19:00 +0530

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -40,7 +40,7 @@ const WorkCard = ({ work }) => {
       <ul className='mt-5 list-disc ml-5 space-y-2'>
         {work.points.map((point, index) => (
           <li
-            key={`experience-point-${index}`}
+            key={`work-point-${index}`}
             className='text-white-100 text-[14px] pl-1 tracking-wider'
           >
             {point}
